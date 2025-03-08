c673899e12243c3ad4716be822e8425c04691d34
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-03-07 22:19:16 -0300

    Tooltip mapa

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabnav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabnav();
 initModal();
+initToolTip();
