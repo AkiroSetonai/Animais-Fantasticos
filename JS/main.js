4ef1aa788c01b0aedc28f13bf4125bfcff1189d5
AkiroSetonai <wallyson.cavalcante@gmail.com>
2025-03-11 10:49:57 -0300

    Fetch-Bitcoin

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,9 @@ import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
 import initDropdownmenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -18,6 +18,6 @@ initModal();
 initToolTip();
 initDropdownmenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
